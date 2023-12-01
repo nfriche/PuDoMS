9e5086d631ca3a7abb15377a1c677491f13f0944
nikita <nikita.friche@gmail.com>
2023-12-01 11:28:17 +0100

    fix monophonic identifier function

--- data/url_merge_mono.py
+++ data/url_merge_mono.py
@@ -4,22 +4,20 @@ import os
 
 ## Function to check if a MIDI file is monophonic
 def is_monophonic(midifile):
-    ## Iterate through each track in the MIDI file
-    for track in midifile.tracks:
-        ## Track the number of notes playing at a time
-        notes_playing = 0
-        ## Iterate through all messages in the track
-        for msg in track:
-            ## Check if the message is a note on and velocity > 0 (note start)
+    active_notes = set()
+    ## Iterate through all messages in the MIDI file
+    for msg in midifile:
+        ## Check if the message is not a meta message (like tempo or time signature)
+        if not msg.is_meta:
+            ## Check that a note is being played
             if msg.type == 'note_on' and msg.velocity > 0:
-                notes_playing += 1
-                ## If more than one note is playing, it's not monophonic
-                if notes_playing > 1:
+                ## Identify whether or not more than one note is playing at a time
+                if len(active_notes) >= 1:
                     return False
-            ## Check if the message is a note off or note on with velocity 0 (note end)
+                active_notes.add(msg.note)
+            ## Check is message is actually a note release and discard it if yes
             elif msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
-                notes_playing = max(0, notes_playing - 1)
-    ## If the track has no polyphony, it's monophonic
+                active_notes.discard(msg.note)
     return True
 
 ## Load the CSV files
@@ -40,13 +38,14 @@ for file in os.listdir(midi_folder_path):
     if file.endswith(".mid") or file.endswith(".midi"):
         path = os.path.join(midi_folder_path, file)
         mid = mido.MidiFile(path)
-        file_number = file.split('.')[0]
         ## Check if file is monophonic and update the dataframe
         if is_monophonic(mid):
             ## Update the 'Monophonic' column based on the File_Number
+            file_number = file.split('.')[0]
             merged_df.loc[merged_df['File_Number'] == file_number, 'Monophonic'] = 'Yes'
         else:
             ## If the file is not monophonic, ensure it's marked 'No'
+            file_number = file.split('.')[0]
             merged_df.loc[merged_df['File_Number'] == file_number, 'Monophonic'] = 'No'
 
 ## Save the updated dataframe to metadata.csv
