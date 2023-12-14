b8b72cb9be22b08301dc31827731f33856f2f153
nikita <nikita.friche@gmail.com>
2023-12-14 17:35:22 +0100

    data split

--- data/split.py
+++ data/split.py
@@ -17,54 +17,79 @@ df['Split'] = 'unassigned'
 df.loc[df['Difficulty'] == 'Advanced', 'Split'] = 'test'
 df.loc[(df['Monophonic'] == 'Yes') | (df['Difficulty'] == 'Beginner'), 'Split'] = 'train'
 
+train_count = df[df['Split'] == 'train'].shape[0]
+print("Number of rows assigned to train:", train_count)
+
 ## Separate the unassigned data
-unassigned_df = df[df['Split'] == 'unassigned']
+unassigned_df = df[df['Split'] == 'unassigned'].copy()
 
-## Perform stratified sampling for the unassigned data
-stratify_cols = ['Composer', 'Genre', 'Difficulty']  ## Modify as needed
-train_val, test_temp = train_test_split(unassigned_df, test_size=0.2, stratify=unassigned_df[stratify_cols])
-train_temp, val_temp = train_test_split(train_val, test_size=0.125, stratify=train_val[stratify_cols])  ## 0.125 x 0.8 = 0.1
+## Fill NaN values in stratify columns with a default value 'Unknown'
+stratify_cols = ['Composer', 'Genre', 'Difficulty']  
+for col in stratify_cols:
+    unassigned_df[col] = unassigned_df[col].fillna('Unknown')
 
-## Assign splits to the stratified samples
+## Check if stratification is possible
+min_members_in_classes = min(unassigned_df[col].value_counts().min() for col in stratify_cols)
+
+if min_members_in_classes >= 2:
+    ## Perform stratified sampling for the unassigned data
+    train_val, test_temp = train_test_split(unassigned_df, test_size=0.2, stratify=unassigned_df[stratify_cols])
+    train_temp, val_temp = train_test_split(train_val, test_size=0.125, stratify=train_val[stratify_cols])  # 0.125 x 0.8 = 0.1
+else:
+    ## Stratification is not possible, use simple random sampling
+    train_val, test_temp = train_test_split(unassigned_df, test_size=0.2)
+    train_temp, val_temp = train_test_split(train_val, test_size=0.125)  
+
+
+## Assign splits
 df.loc[train_temp.index, 'Split'] = 'train'
 df.loc[val_temp.index, 'Split'] = 'validation'
 df.loc[test_temp.index, 'Split'] = 'test'
 
-## Function to adjust the splits to 80/10/10
-def adjust_splits(df, target_ratios):
-    counts = df['Split'].value_counts(normalize=True)
-    total = len(df)
-    target_counts = {split: int(total * ratio) for split, ratio in target_ratios.items()}
 
+def adjust_splits(df, target_counts, original_split_info):
+    # Iterate through each split and adjust to meet the target counts
     for split, target_count in target_counts.items():
-        current_count = counts.get(split, 0)
+        current_count = df[df['Split'] == split].shape[0]
         difference = target_count - current_count
 
         if difference > 0:
-            ## Need to add more to this split
-            pool = df[df['Split'] == 'unassigned']
+            # Need to add more rows to this split
+            # Select rows from the originally unassigned rows
+            pool = original_split_info[(original_split_info['Split'] == 'unassigned') & (df['Split'] == 'unassigned')]
             if not pool.empty:
                 additional = pool.sample(min(len(pool), difference), random_state=0)
                 df.loc[additional.index, 'Split'] = split
+
         elif difference < 0:
-            ## Need to remove some from this split
-            pool = df[df['Split'] == split]
+            # Need to remove some rows from this split
+            # Select rows that are currently in this split but were originally unassigned
+            pool = original_split_info[original_split_info['Split'] == split]
             if not pool.empty:
                 to_remove = pool.sample(min(len(pool), -difference), random_state=0)
                 df.loc[to_remove.index, 'Split'] = 'unassigned'
 
-## Define target ratios for the splits
+    # Assign remaining unassigned rows to the splits that haven't reached their target counts
+    remaining_unassigned = df[df['Split'] == 'unassigned']
+    for split, target_count in target_counts.items():
+        current_count = df[df['Split'] == split].shape[0]
+        difference = target_count - current_count
+
+        if not remaining_unassigned.empty and difference > 0:
+            additional = remaining_unassigned.sample(min(len(remaining_unassigned), difference), random_state=0)
+            df.loc[additional.index, 'Split'] = split
+            remaining_unassigned = remaining_unassigned.drop(additional.index)
+
+# Store the original split information for unassigned rows
+original_split_info = df.copy()
+
+# Define target ratios and calculate target counts
+total_rows = len(df)
 target_ratios = {'train': 0.8, 'validation': 0.1, 'test': 0.1}
+target_counts = {split: int(total_rows * ratio) for split, ratio in target_ratios.items()}
 
-## Apply the adjustment function to the dataframe
-adjust_splits(df, target_ratios)
+# Apply the adjustment function
+adjust_splits(df, target_counts, original_split_info)
 
-## Handle any remaining unassigned (due to rounding issues or constraints)
-## Assign them to the smallest split to balance it out
-remaining = df[df['Split'] == 'unassigned']
-if not remaining.empty:
-    smallest_split = df['Split'].value_counts().idxmin()
-    df.loc[remaining.index, 'Split'] = smallest_split
-
-## Save the updated dataframe
+# Save the updated dataframe
 df.to_csv('updated_metadata_with_split.csv', index=False)
