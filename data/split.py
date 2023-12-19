9e8b579bf281b9613cc7006d259bd596734a5160
nikita <nikita.friche@gmail.com>
2023-12-19 12:12:14 +0100

    splitting fixed

--- data/split.py
+++ data/split.py
@@ -12,9 +12,9 @@ df_filtered = df[(df['Instruments'].isin(['Piano', 'Piano (2)', 'Piano (3)'])) &
 
 # Calculate the exact number of files for each split
 total_files = len(df_filtered)
-train_count = 5719
-val_count = 715
-test_count = 715
+train_count = 5663
+val_count = 708
+test_count = 708
 
 # Perform the split for the filtered dataset
 train_val, test_set = train_test_split(df_filtered, test_size=test_count, random_state=42)
@@ -22,7 +22,7 @@ train_set, val_set = train_test_split(train_val, test_size=val_count, random_sta
 
 # Update the 'Split' column in the original dataframe
 df.loc[train_set.index, 'Split'] = 'train'
-df.loc[val_set.index, 'Split'] = 'val'
+df.loc[val_set.index, 'Split'] = 'validation'
 df.loc[test_set.index, 'Split'] = 'test'
 
 # Save the updated dataframe to a new CSV file
