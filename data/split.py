1f59ca4ab1ade07353d7787dca3fe71a484bceef
nikita <nikita.friche@gmail.com>
2024-01-05 18:30:16 +0100

    updated split, removed silent files

--- data/split.py
+++ data/split.py
@@ -1,29 +1,34 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# Load the dataset
+## Load the metadata file
 df = pd.read_csv('metadata.csv')
 
-# Add a new column for the split and initially set all to 'excluded'
+## Add a new column for the split and initially set all to 'excluded'
 df['Split'] = 'excluded'
 
-# Filter based on the 'Instruments' and 'Download_Status' columns
-df_filtered = df[(df['Instruments'].isin(['Piano', 'Piano (2)', 'Piano (3)'])) & (df['Download_Status'] != 'FAILED')]
+## Filter based on the 'Instruments', 'Download_Status', and 'Silent' columns
+df_filtered = df[
+    (df['Instruments'].isin(['Piano', 'Piano (2)', 'Piano (3)'])) 
+    & (df['Download_Status'] != 'FAILED')
+    & (df['Silent'] != 'Yes')  
+]
 
-# Calculate the exact number of files for each split
+## Calculate the exact number of files for each split
 total_files = len(df_filtered)
-train_count = 5663
-val_count = 708
-test_count = 708
+#print(total_files)
+train_count = 5614
+val_count = 702
+test_count = 702
 
-# Perform the split for the filtered dataset
+## Perform the split for the filtered dataset
 train_val, test_set = train_test_split(df_filtered, test_size=test_count, random_state=42)
 train_set, val_set = train_test_split(train_val, test_size=val_count, random_state=42)
 
-# Update the 'Split' column in the original dataframe
+## Update the 'Split' column in the original dataframe
 df.loc[train_set.index, 'Split'] = 'train'
 df.loc[val_set.index, 'Split'] = 'validation'
 df.loc[test_set.index, 'Split'] = 'test'
 
-# Save the updated dataframe to a new CSV file
+## Save the updated dataframe to a new CSV file
 df.to_csv('updated_metadata_with_split.csv', index=False)
